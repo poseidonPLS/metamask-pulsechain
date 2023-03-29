9fb25078e83e2258db4f794e94f86c756f31d08d
donseelim <donseelim@gmail.com>
2023-03-29 14:09:24 +0800

    Made a custom search function

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import logo from './logo.png'; // Import the logo image
 
 function App() {
   const [account, setAccount] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchInput, setSearchInput] = useState('');
+
 
   const connectMetaMask = async () => {
     const provider = await detectEthereumProvider();
@@ -44,7 +47,34 @@ function App() {
     }
   };
 
-  const addCustomToken = async (tokenAddress, tokenSymbol, tokenDecimals) => {
+  const searchTokens = async (symbol) => {
+    const query = `
+      {
+        tokens (where: {symbol_contains_nocase: "${symbol}"}) {
+          id,
+          symbol,
+          decimals
+        }
+      }
+    `;
+  
+    try {
+      const response = await fetch('https://graph.v3.testnet.pulsechain.com/subgraphs/name/pulsechain/pulsex', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ query }),
+      });
+  
+      const data = await response.json();
+      setSearchResults(data.data.tokens);
+    } catch (error) {
+      console.error('Error fetching token data:', error);
+    }
+  };
+
+  const addCustom = async (Address, Symbol, Decimals) => {
     const provider = await detectEthereumProvider();
 
     if (provider) {
@@ -53,15 +83,15 @@ function App() {
 
       if (currentChainId === pulseChainTestnetV3ChainId) {
         try {
-          // Request to add the custom token to MetaMask
+          // Request to add the custom  to MetaMask
           await provider.request({
             method: 'wallet_watchAsset',
             params: {
               type: 'ERC20',
               options: {
-                address: tokenAddress,
-                symbol: tokenSymbol,
-                decimals: tokenDecimals,
+                address: Address,
+                symbol: Symbol,
+                decimals: Decimals,
               },
             },
           });
@@ -69,7 +99,7 @@ function App() {
           console.error(error);
         }
       } else {
-        console.error('Please switch to the PulseChain Testnet V3 before adding tokens.');
+        console.error('Please switch to the PulseChain Testnet V3 before adding s.');
       }
     } else {
       console.log('Please install MetaMask!');
@@ -84,12 +114,34 @@ function App() {
         <div className="button-row">
           <button onClick={connectMetaMask}>Set up PulseChain in MetaMask</button>
         </div>
-        <div className="button-row">
-          <button onClick={() => addCustomToken('0x3b34fff74497ca21771ce9a0c11cb07490686a58', 'PLSX', 18)}>Add PulseX Token</button>
-        </div>
-        <div className="button-row">
-          <button onClick={() => addCustomToken('0x2b591e99afe9f32eaa6214f7b7629768c40eeb39', 'HEX', 8)}>Add HEX Token</button>
-        </div>
+        <div className="grid-container">
+      <button onClick={() => addCustom('0x3b34fff74497ca21771ce9a0c11cb07490686a58', 'PLSX', 18)}>Add PLSX </button>
+      <button onClick={() => addCustom('0x2b591e99afe9f32eaa6214f7b7629768c40eeb39', 'HEX', 8)}>Add HEX </button>
+      <button onClick={() => addCustom('0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', 'USDC', 6)}>Add USDC </button>
+      <button onClick={() => addCustom('0x6b175474e89094c44da98b954eedeac495271d0f', 'DAI', 18)}>Add DAI </button>
+      <button onClick={() => addCustom('0xdac17f958d2ee523a2206206994597c13d831ec7', 'USDT', 6)}>Add USDT </button>
+    </div>
+    <div className="button-row">
+    <input
+  type="text"
+  placeholder="Search for other tokens"
+  onChange={(e) => setSearchInput(e.target.value)}
+  style={{ fontSize: '1rem', padding: '0.5rem', width: '60%', marginRight: '1rem' }}
+/>
+      <button onClick={() => searchTokens(searchInput)}>Search</button>
+    </div>
+    <div className="search-results grid-container">
+  {searchResults.map((token) => (
+    <div key={token.id} className="token">
+      <h4>{token.symbol}</h4>
+      <p className="contract-address">{token.id}</p>
+      <button onClick={() => addCustom(token.id, token.symbol, token.decimals)}>
+        Add {token.symbol} Token
+      </button>
+    </div>
+  ))}
+</div>
+
         {account ? <p>Connected Account: {account}</p> : <p>Not connected</p>}
       </header>
       <footer className="App-footer">

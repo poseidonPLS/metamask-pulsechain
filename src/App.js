054b8ce06ee940ea265e6c8a3b798c1a2e8a1ab7
donseelim <donseelim@gmail.com>
2023-04-27 22:44:26 +0800

    Fix

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
         const currentChainId = await provider.request({
           method: "eth_chainId",
         });
-        const pulseChaintestnetChainId = "0x3af"; // Chain ID: 369
+        const pulseChaintestnetChainId = "0x3af"; // Chain ID: 943
 
         if (currentChainId !== pulseChaintestnetChainId) {
           // Add PulseChain configuration
@@ -33,8 +33,8 @@ function App() {
             chainId: pulseChaintestnetChainId,
             chainName: "PulseChain",
             nativeCurrency: {
-              name: "PLS",
-              symbol: "PLS",
+              name: "tPLS",
+              symbol: "tPLS",
               decimals: 18,
             },
             rpcUrls: ["https://rpc.v4.testnet.pulsechain.com"], // RPC URL
